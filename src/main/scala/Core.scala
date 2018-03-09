8c20dc701e045fd438dcc8b26bf91c9e8eeeb363
Luís Fiolhais <luisazenhas.fiolhais@openmailbox.org>
2018-03-09 14:19:59 +0000

    Connect the ALU, idecoder and regfile modules

--- src/main/scala/Core.scala
+++ src/main/scala/Core.scala
@@ -10,11 +10,23 @@ import adept.registerfile.RegisterFile
 class Adept(config: AdeptConfig) extends Module {
   val io = IO(new Bundle{})
 
+  //////////////////////////////////////////////////////////////////////////////
   // Create Modules
+  //////////////////////////////////////////////////////////////////////////////
   // Instruction Decoder
   val idecode = Module(new InstructionDecoder(config))
 
   // Register File
   // Creates a register file with XLen XLen-bit registers
   val register_file = Module(new RegisterFile(config))
+
+  // ALU
+  val alu = Module(new ALU(config))
+
+  //////////////////////////////////////////////////////////////////////////////
+  // Connections
+  //////////////////////////////////////////////////////////////////////////////
+  register_file.decoder <> idecode.registers
+  alu.registers <> register_file.registers
+  alu.decoder_params <> idecode.alu
 }
