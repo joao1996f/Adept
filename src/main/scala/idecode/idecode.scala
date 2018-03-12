98c1a4a6e19b249cd744d5d1d56432a9ec830bb4
Luís Fiolhais <luisazenhas.fiolhais@openmailbox.org>
2018-03-12 17:19:37 +0000

    Add operand select control signals to the idecoder
    Add operand_a select signal to choose between pc and rs1. The immediate
    is always passed to the ALU to facilitate the arithmetic operations.
    This may change down the line.
    
    Add mux select for the write back stage.
    
    Add another immediate which is only sent to the instruction fetch stage.
    This is why it may be a bad design decision to add an immediate input to
    the ALU...
    
    Add a we for the memory
    
    There is still no decoding for the FENCE, CSR and E instructions. These
    will be added later down line. Currently, we are focusing on the basics.
    This is bad code and I apologize. But right now I just to see if it
    works.

--- src/main/scala/idecode/idecode.scala
+++ src/main/scala/idecode/idecode.scala
@@ -29,6 +29,9 @@ class DecoderRegisterOut(val config: AdeptConfig) extends Bundle {
   }
 }
 
+////////////////////////////////////////////////////////////////////////////////
+// BE WARNED! THIS IS TERRIBLE CODE, READ AT YOUR OWN PERIL!
+////////////////////////////////////////////////////////////////////////////////
 class InstructionDecoder(config: AdeptConfig) extends Module {
   val io = IO(new Bundle{
                 // Input
@@ -37,6 +40,10 @@ class InstructionDecoder(config: AdeptConfig) extends Module {
                 // Output
                 val registers = new DecoderRegisterOut(config)
                 val alu       = new DecoderALUOut(config)
+                val sel_operand_b = Output(UInt(1.W))
+                val sel_rf_wb     = Output(UInt(1.W))
+                val imm_b_offset  = Output(SInt(config.XLen.W))
+                val mem_we        = Output(Bool())
               })
 
   // BTW this is a bad implementation, but its OK to start off.
@@ -59,9 +66,19 @@ class InstructionDecoder(config: AdeptConfig) extends Module {
     io.registers.rsd_sel := rsd_sel
     // Shift instructions have a special code in the immediate, in the ALU check
     // the two LSBs of the OP
-    io.alu.imm      := imm
-    io.alu.op       := op
-    io.registers.we := true.B
+    io.alu.imm       := imm
+    io.alu.op        := op
+    io.imm_b_offset  := 0.U
+    io.registers.we  := true.B
+    io.sel_operand_a := 0.U
+    io.mem_we        := false.B
+    // Selects the ALU result to be written to the Register File when it is not
+    // a load instruction
+    when (op_code =/= "b0000011".U) {
+      io.sel_rf_wb     := 0.U
+    } .otherwise {
+      io.sel_rf_wb     := 1.U
+    }
   } .elsewhen (op_code === "b0110011".U) {
     // R-Type Decode => OP Code: 0110011 of instruction
     io.registers.rs1_sel := rs1_sel
@@ -71,7 +88,12 @@ class InstructionDecoder(config: AdeptConfig) extends Module {
     // the ALU check the two LSBs of the OP
     io.alu.imm      := imm
     io.alu.op       := op
+    io.imm_b_offset := 0.U
     io.registers.we := true.B
+    // Select RS1 and write the ALU result to the register file
+    io.sel_operand_a := 0.U
+    io.sel_rf_wb     := 0.U
+    io.mem_we        := false.B
   } .elsewhen (op_code === "b0100011".U) {
     // S-Type Decode => OP Code: 0100011 of instruction
     io.registers.rs1_sel := rs1_sel
@@ -80,33 +102,61 @@ class InstructionDecoder(config: AdeptConfig) extends Module {
     io.alu.imm           := Cat(imm(11, 5), rsd_sel)
     io.alu.op            := op
     io.registers.we      := false.B
+    io.imm_b_offset      := 0.U
+    io.sel_operand_a     := 0.U
+    // This is don't care. Register File write enable is set to false
+    io.sel_rf_wb         := 0.U
+    io.mem_we            := true.B
   } .elsewhen (op_code === "b1100011") {
     // B-Type Decode => OP Code: 1100011 of instruction
     io.registers.rs1_sel := rs1_sel
     io.registers.rs2_sel := rs2_sel
     io.registers.rsd_sel := 0.U
-    io.alu.imm           := Cat(imm(11), rsd_sel(0), imm(10, 5), rsd_sel(4, 1), 0.asUInt(1.W))
-    io.alu.op            := op
+    io.imm_b_offset      := Cat(imm(11), rsd_sel(0), imm(10, 5), rsd_sel(4, 1), 0.asUInt(1.W))
+    io.alu.imm           := 1024.U
+
+    when (op === "b000".U || op === "b001".U) {
+      io.alu.op := "b000".U
+    } .elsewhen (op === "b100".U || op === "b101".U) {
+      io.alu.op := "b010".U
+    } .otherwise {
+      io.alu.op := "b011".U
+    }
+
     io.registers.we      := false.B
+    io.sel_operand_a     := 0.U
+    // This is don't care. Register File write enable is set to false
+    io.sel_rf_wb         := 0.U
+    io.mem_we            := false.B
   } .elsewhen (op_code === "b0010111".U || op_code === "b0110111".U) {
     // U-Type Decode => OP Code: 0010111 or 0110111 of instruction
     io.registers.rs1_sel := 0.U
     io.registers.rs2_sel := 0.U
     io.registers.rsd_sel := rsd_sel
-    io.alu.imm           := Cat(imm, rs1_sel, op)
+    io.alu.imm           := Cat(imm, rs1_sel, op, Fill(12, "b0".U))
     io.alu.op            := 0.U
-    when (op_code(4) === false.B) {
+    io.imm_b_offset      := 0.U
+    when (op_code(5) === false.B) {
       io.registers.we := false.B
     } .otherwise {
       io.registers.we := true.B
     }
+    io.sel_operand_a     := 1.U
+    // This is don't care. Register File write enable is set to false
+    io.sel_rf_wb         := 0.U
+    io.mem_we            := false.B
   } .otherwise {
     // J-Type Decode => OP Code: 1101111 of instruction
     io.registers.rs1_sel := 0.U
     io.registers.rs2_sel := 0.U
     io.registers.rsd_sel := rsd_sel
+    io.imm_b_offset      := 0.U
     io.alu.imm           := Cat(imm(11), rs1_sel, op, imm(0), imm(10, 1), 0.asUInt(1.W))
     io.alu.op            := 0.U
     io.registers.we      := true.B
+    io.sel_operand_a     := 0.U
+    // This is don't care. Register File write enable is set to false
+    io.sel_rf_wb         := 0.U
+    io.mem_we            := false.B
   }
 }
