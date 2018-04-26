28bdc349796f4e02717b15a2a8dad9b12393ae13
Luís Fiolhais <luisazenhas.fiolhais@openmailbox.org>
2018-04-26 17:58:03 +0100

    Refactor Load tests

---
+++ src/test/scala/mem/load_tests.scala
@@ -0,0 +1,132 @@
+package adept.mem.tests
+
+import chisel3.iotesters
+import chisel3.iotesters.{ChiselFlatSpec, Driver, PeekPokeTester}
+
+import scala.collection.mutable.HashMap
+
+import adept.mem.Memory
+import adept.mem.BaseMemory
+import adept.config.AdeptConfig
+
+class BaseLoad(c: Memory, config: AdeptConfig) extends BaseMemory(c, config) {
+  final val LB_OP_CODE = 0;
+  final val LH_OP_CODE = 1;
+  final val LW_OP_CODE = 2;
+  final val LBU_OP_CODE = 4;
+  final val LHU_OP_CODE = 5;
+
+  def setSignals(op: Int, addr: Int) {
+    poke(c.io.decode.op, op)
+    poke(c.io.in.addr, addr)
+    poke(c.io.decode.en, true)
+    poke(c.io.decode.we, false)
+  }
+
+  def getFinalRead(addr: Int, opType: Int) : (Int, Boolean) = {
+    val lsbs = addr & 0x00000003
+    val masked_addr = addr >>> 2
+
+    val bitMask = opType match {
+      case LB_OP_CODE | LBU_OP_CODE => 0x000000ff << (8 * lsbs)
+      case LH_OP_CODE | LHU_OP_CODE if lsbs < 3 => 0x0000ffff << (8 * lsbs)
+      case LW_OP_CODE if lsbs == 0 => 0xffffffff
+      // TODO: Memory should throw a trap for an ilegal memory op
+      case _ => 0x00000000
+    }
+
+    val result = lsbs match {
+      case 0 if bitMask != 0 => (mem_img(masked_addr) & bitMask, true)
+      // TODO: Memory should throw a trap for an ilegal memory access
+      case _ => (0, false)
+    }
+
+    return result
+  }
+
+  def getSignExtend(read: Int, opType: Int) : Int = {
+    val bitMask = opType match {
+      case LB_OP_CODE | LBU_OP_CODE => 0x00000080
+      case LH_OP_CODE | LHU_OP_CODE => 0x00008000
+      case _ => 0x00000000
+    }
+
+    val shift = scala.math.log(bitMask) / scala.math.log(2)
+
+    val signExtend = if (((read & bitMask) >>> shift.toInt) == 1) {
+      ~(bitMask | (bitMask - 1))
+    } else {
+      0x00000000
+    }
+
+    return signExtend
+  }
+}
+
+class LoadWord(c: Memory, config: AdeptConfig) extends BaseLoad(c, config) {
+  private def LW(addr: Int, mem_img: HashMap[Int, Int]) = {
+    setSignals(LW_OP_CODE, addr)
+
+    // Ignore output while stall is active and advance simulation
+    do {
+      step(1)
+    } while (peek(c.io.stall) == 1)
+
+    val finalRead = getFinalRead(addr, LW_OP_CODE)
+
+    if (finalRead._2) {
+      expect(c.io.data_out, finalRead._1)
+    }
+  }
+
+  for (i <- 0 until 100) {
+    val addr = rnd.nextInt(5000)
+    LW(addr, mem_img)
+  }
+}
+
+class LoadHalf(c: Memory, config: AdeptConfig) extends BaseLoad(c, config) {
+  private def LH(addr: Int, mem_img: HashMap[Int, Int]) = {
+    setSignals(LH_OP_CODE, addr)
+
+    // Ignore output while stall is active and advance simulation
+    do {
+      step(1)
+    } while (peek(c.io.stall) == 1)
+
+    val finalRead = getFinalRead(addr, LH_OP_CODE)
+
+    if (finalRead._2) {
+      val signExtend = getSignExtend(finalRead._1, LH_OP_CODE)
+      expect(c.io.data_out, signExtend | finalRead._1)
+    }
+  }
+
+  for (i <- 0 until 100) {
+    val addr = rnd.nextInt(5000)
+    LH(addr, mem_img)
+  }
+}
+
+class LoadByte(c: Memory, config: AdeptConfig) extends BaseLoad(c, config) {
+  private def LB(addr: Int, mem_img: HashMap[Int, Int]) = {
+    setSignals(LB_OP_CODE, addr)
+
+    // Ignore output while stall is active and advance simulation
+    do {
+      step(1)
+    } while (peek(c.io.stall) == 1)
+
+    val finalRead = getFinalRead(addr, LB_OP_CODE)
+
+    if (finalRead._2) {
+      val signExtend = getSignExtend(finalRead._1, LB_OP_CODE)
+      expect(c.io.data_out, signExtend | finalRead._1)
+    }
+  }
+
+  for (i <- 0 until 100) {
+    val addr = rnd.nextInt(5000)
+    LB(addr, mem_img)
+  }
+}
--- src/test/scala/mem/memory.scala
+++ src/test/scala/mem/memory.scala
@@ -1,11 +1,12 @@
 package adept.mem
 
-import scala.collection.mutable.HashMap
-
 import chisel3.iotesters
 import chisel3.iotesters.{ChiselFlatSpec, Driver, PeekPokeTester}
 
 import adept.config.AdeptConfig
+import adept.mem.tests._
+
+import scala.collection.mutable.HashMap
 
 class StallLogic(c: Memory) extends PeekPokeTester(c) {
   var counter = 0
@@ -75,111 +76,6 @@ class BaseMemory(c: Memory, config: AdeptConfig) extends PeekPokeTester(c) {
   val mem_img = writeGarbage(c)
 }
 
-class LoadWord(c: Memory, config: AdeptConfig) extends BaseMemory(c, config) {
-  private def LW(addr: Int, mem_img: HashMap[Int, Int]) = {
-    poke(c.io.decode.op, 2)
-    poke(c.io.in.addr, addr)
-    poke(c.io.decode.en, true)
-    poke(c.io.decode.we, false)
-
-    // Ignore output while stall is active and advance simulation
-    do {
-      step(1)
-    } while (peek(c.io.stall) == 1)
-
-    val lsbs = addr & 0x00000003
-    val masked_addr = addr >>> 2
-    val final_read = lsbs match {
-      case 0 => (mem_img(masked_addr), true)
-      // TODO: Memory should throw a trap for an ilegal memory access
-      case _ => (0, false)
-    }
-
-    if (final_read._2) {
-      expect(c.io.data_out, final_read._1)
-    }
-  }
-
-  for (i <- 0 until 100) {
-    val addr = rnd.nextInt(5000)
-    LW(addr, mem_img)
-  }
-}
-
-class LoadHalf(c: Memory, config: AdeptConfig) extends BaseMemory(c, config) {
-  private def LH(addr: Int, mem_img: HashMap[Int, Int]) = {
-    poke(c.io.decode.op, 1)
-    poke(c.io.in.addr, addr)
-    poke(c.io.decode.en, true)
-    poke(c.io.decode.we, false)
-
-    // Ignore output while stall is active and advance simulation
-    do {
-      step(1)
-    } while (peek(c.io.stall) == 1)
-
-    val lsbs = addr & 0x00000003
-    val masked_addr = addr >>> 2
-    val final_read = lsbs match {
-      case 0 => (mem_img(masked_addr) & 0x0000ffff, true)
-      case 1 => ((mem_img(masked_addr) & 0x00ffff00) >>> 8, true)
-      case 2 => ((mem_img(masked_addr) & 0xffff0000) >>> 16, true)
-      // TODO: Memory should throw a trap for an ilegal memory access
-      case _ => (0, false)
-    }
-
-    val sign_extend = if (((final_read._1 & 0x00008000) >>> 15) == 1) {
-      0xffff0000
-    } else {
-      0x00000000
-    }
-
-    if (final_read._2) {
-      expect(c.io.data_out, sign_extend | final_read._1)
-    }
-  }
-
-  for (i <- 0 until 100) {
-    val addr = rnd.nextInt(5000)
-    LH(addr, mem_img)
-  }
-}
-
-class LoadByte(c: Memory, config: AdeptConfig) extends BaseMemory(c, config) {
-  private def LB(addr: Int, mem_img: HashMap[Int, Int]) = {
-    poke(c.io.decode.op, 0)
-    poke(c.io.in.addr, addr)
-    poke(c.io.decode.en, true)
-    poke(c.io.decode.we, false)
-
-    // Ignore output while stall is active and advance simulation
-    do {
-      step(1)
-    } while (peek(c.io.stall) == 1)
-
-    val lsbs = addr & 0x00000003
-    val masked_addr = addr >>> 2
-    val final_read = lsbs match {
-      case 0 => mem_img(masked_addr) & 0x000000ff
-      case 1 => (mem_img(masked_addr) & 0x0000ff00) >>> 8
-      case 2 => (mem_img(masked_addr) & 0x00ff0000) >>> 16
-      case 3 => (mem_img(masked_addr) & 0xff000000) >>> 24
-      case _ => 0
-    }
-    val sign_extend = if (((final_read & 0x00000080) >>> 7) == 1) {
-      0xffffff00
-    } else {
-      0x00000000
-    }
-
-    expect(c.io.data_out, sign_extend | final_read)
-  }
-
-  for (i <- 0 until 100) {
-    val addr = rnd.nextInt(5000)
-    LB(addr, mem_img)
-  }
-}
 
 class MemoryUnitTester(c: Memory, config: AdeptConfig) extends PeekPokeTester(c) {
   new StallLogic(c)
