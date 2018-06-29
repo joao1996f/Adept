e9e86181e6ea70cda0dc83db9a1dfec84aa73a95
Luís Fiolhais <lfiolhais@iobundle.com>
2018-06-29 13:28:09 +0100

    Travis: send artifacts print to stdout to avoid TO

--- scripts/artifacts.sh
+++ scripts/artifacts.sh
@@ -5,7 +5,21 @@ mkdir artifacts
 mv logs/ test_run_dir/ verilog/ artifacts/
 export ARTIFACT_NAME=$(date +%d-%m-%Y--%H-%M-%S)
 tar -czf $ARTIFACT_NAME.tgz artifacts
+
 export SSHPASS=$DEPLOY_PASS1
-sshpass -e scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -P $DEPLOY_PORT1 $ARTIFACT_NAME.tgz $DEPLOY_USER1@$DEPLOY_HOST:$DEPLOY_PATH1
+sshpass -e scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -P $DEPLOY_PORT1 $ARTIFACT_NAME.tgz $DEPLOY_USER1@$DEPLOY_HOST:$DEPLOY_PATH1 &
+
+export PID=$!
+while [[ `ps -p $PID | tail -n +2` ]]; do
+    echo 'Sending Artifacts to Server 1'
+    sleep 540
+done
+
 export SSHPASS=$DEPLOY_PASS2
-sshpass -e scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -P $DEPLOY_PORT2 $ARTIFACT_NAME.tgz $DEPLOY_USER2@$DEPLOY_HOST:$DEPLOY_PATH2
+sshpass -e scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -P $DEPLOY_PORT2 $ARTIFACT_NAME.tgz $DEPLOY_USER2@$DEPLOY_HOST:$DEPLOY_PATH2 &
+
+export PID=$!
+while [[ `ps -p $PID | tail -n +2` ]]; do
+    echo 'Sending Artifacts to Server 2'
+    sleep 540
+done
--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,1 +1,13 @@
 #!/usr/bin/env bash
+sudo apt -qq update
+sudo apt install -y sshpass
+mkdir riscv-tools
+export SSHPASS=$DEPLOY_PASS2
+sshpass -e scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -P $DEPLOY_PORT2 $DEPLOY_USER2@$DEPLOY_HOST:$DEPLOY_PATH2/../buntu-home/toolchain.tar.gz riscv-tools/
+cd riscv-tools/
+tar -xvzf toolchain.tar.gz
+cd ..
+./scripts/install-deps.sh
+echo "RISCV: $RISCV"
+echo "PATH: $PATH"
+echo "PWD: $PWD"
