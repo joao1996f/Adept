d607e53e1ea9e433edfa0693b80ba94cb5472dae
Luís Fiolhais <lfiolhais@iobundle.com>
2018-06-29 10:26:44 +0100

    Disable host public key check

--- scripts/artifacts.sh
+++ scripts/artifacts.sh
@@ -6,6 +6,6 @@ mv logs/ test_run_dir/ verilog/ artifacts/
 export ARTIFACT_NAME=$(date +%d-%m-%Y--%H-%M-%S)
 tar -czf $ARTIFACT_NAME.tgz artifacts
 export SSHPASS=$DEPLOY_PASS1
-sshpass -e scp -P $DEPLOY_PORT1 $ARTIFACT_NAME.tgz $DEPLOY_USER1@$DEPLOY_HOST:$DEPLOY_PATH1
+sshpass -e scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -P $DEPLOY_PORT1 $ARTIFACT_NAME.tgz $DEPLOY_USER1@$DEPLOY_HOST:$DEPLOY_PATH1
 export SSHPASS=$DEPLOY_PASS2
-sshpass -e scp -P $DEPLOY_PORT2 $ARTIFACT_NAME.tgz $DEPLOY_USER2@$DEPLOY_HOST:$DEPLOY_PATH2
+sshpass -e scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -P $DEPLOY_PORT2 $ARTIFACT_NAME.tgz $DEPLOY_USER2@$DEPLOY_HOST:$DEPLOY_PATH2
