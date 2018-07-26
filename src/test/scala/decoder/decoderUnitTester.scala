d275fe6ef6804032a95a5b3ae93f27337f621604
Valter Mário <vjbmario@hotmail.com>
2018-07-26 19:15:13 +0100

    Create SLTI Chisel flat spec tests for decoder
    It adds the specific test for the instruction SLTI to the chisel flat
    spec tests for the decoder.

--- src/test/scala/decoder/decoderUnitTester.scala
+++ src/test/scala/decoder/decoderUnitTester.scala
@@ -10,6 +10,7 @@ import adept.decoder.tests.imm._
 class DecoderTestBase(c: InstructionDecoder) extends PeekPokeTester(c) {
   val op_code = new OpCodes
   val slli = Integer.parseInt("001", 2)
+  val slti = Integer.parseInt("010", 2)
 
   def signExtension (imm: Int, nbits: Int) : Int = {
     if ((imm >> (nbits-1)) == 1) {
@@ -23,19 +24,25 @@ class DecoderTestBase(c: InstructionDecoder) extends PeekPokeTester(c) {
 class DecoderUnitTesterAll(e: InstructionDecoder) extends PeekPokeTester(e) {
     // Immediate Type Instructions
     new ADDI(e)
+    new SLTI(e)
     new SLLI(e)
 }
 
 class DecoderTester extends ChiselFlatSpec {
   // Generate configuration
   val config = new AdeptConfig
-  
+
   ///////////////////////////////////////////////////////////////////////////
   // Immediate Type Instructions
   ////////////////////////////////////////////////////////////////////////////
   "Decoder" should s"test ADDI instruction (with verilator)" in {
     Driver(() => new InstructionDecoder(config), "verilator") {
       e => new ADDI(e)
+    } should be (true)
+  }
+  "Decoder" should s"test SLTI instruction (with verilator)" in {
+    Driver(() => new InstructionDecoder(config), "verilator") {
+      e => new SLTI(e)
     } should be (true)
   }
   "Decoder" should s"test SLLI instruction (with verilator)" in {
--- src/test/scala/decoder/imm_tests.scala
+++ src/test/scala/decoder/imm_tests.scala
@@ -14,6 +14,7 @@ class ADDI(c: InstructionDecoder) extends DecoderTestBase(c) {
   private def ADDI(rs1: Int, imm: Int, rd: Int) {
     val instr = ((imm << 20) | ((31 & rs1) << 15) | ((31 & rd) << 7) | op_code.Immediate.litValue())
     val new_imm = signExtension(imm, 12)
+
     poke(c.io.stall_reg, false)
     poke(c.io.basic.instruction, instr)
 
@@ -31,18 +32,46 @@ class ADDI(c: InstructionDecoder) extends DecoderTestBase(c) {
   }
 
   for (i <- 0 until 100) {
-    var rs1 = rnd.nextInt(32)
-    var imm = rnd.nextInt(4096)
+    val rs1 = rnd.nextInt(32)
+    val imm = rnd.nextInt(4096)
     val rd  = rnd.nextInt(32)
 
     ADDI(rs1, imm, rd)
   }
 }
 
+class SLTI(c: InstructionDecoder) extends DecoderTestBase(c) {
+  private def SLTI(rs1: Int, imm: Int, rd: Int) {
+    val instr = ((imm << 20) | ((31 & rs1) << 15) | (slti << 12) | ((31 & rd) << 7) | op_code.Immediate.litValue())
+    val new_imm = signExtension(imm, 12)
+    poke(c.io.stall_reg, false)
+    poke(c.io.basic.instruction, instr)
+
+    step(1)
+
+    expect(c.io.basic.out.registers.we, true)
+    expect(c.io.basic.out.registers.rsd_sel, rd)
+    expect(c.io.basic.out.registers.rs1_sel, rs1)
+    expect(c.io.basic.out.immediate, new_imm)
+    expect(c.io.basic.out.trap, 0)
+    expect(c.io.basic.out.alu.op, AluOps.slt)
+    expect(c.io.basic.out.sel_rf_wb, AdeptControlSignals.result_alu)
+    expect(c.io.basic.out.sel_operand_a, AdeptControlSignals.sel_oper_A_rs1)
+    expect(c.io.basic.out.sel_operand_b, AdeptControlSignals.sel_oper_B_imm)
+  }
+
+  for (i <- 0 until 100) {
+    val rs1 = rnd.nextInt(32)
+    val imm = rnd.nextInt(4096)
+    val rd  = rnd.nextInt(32)
+
+    SLTI(rs1, imm, rd)
+  }
+}
 
 class SLLI(c: InstructionDecoder) extends DecoderTestBase(c) {
   private def SLLI(rs1: Int, imm: Int, rd: Int) {
-    val instr = ((imm << 20) | ((31 & rs1) << 15) | (slli << 12) | ((31 & rd) << 7) | op_code.Immediate.litValue())   
+    val instr = ((imm << 20) | ((31 & rs1) << 15) | (slli << 12) | ((31 & rd) << 7) | op_code.Immediate.litValue())
     val shamt = 31 & imm
     val new_imm = signExtension (imm, 12)
     val trap = if ((imm >> 5) != 0) {
@@ -50,7 +79,8 @@ class SLLI(c: InstructionDecoder) extends DecoderTestBase(c) {
                } else {
                  0
                }
-    poke(c.io.stall_reg, false)                 
+
+    poke(c.io.stall_reg, false)
     poke(c.io.basic.instruction, instr)
 
     step(1)
@@ -68,10 +98,10 @@ class SLLI(c: InstructionDecoder) extends DecoderTestBase(c) {
   }
 
   for (i <- 0 until 100) {
-    var rs1 = rnd.nextInt(32)
-    var imm = rnd.nextInt(4096)
+    val rs1 = rnd.nextInt(32)
+    val imm = rnd.nextInt(4096)
     val rd  = rnd.nextInt(32)
-    
+
     SLLI(rs1, imm, rd)
   }
 }
