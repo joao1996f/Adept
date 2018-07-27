fb672a3db16537e4b7c07c08c02a052c1c77e822
Luís Fiolhais <lfiolhais@iobundle.com>
2018-07-27 16:45:47 +0100

    Travis: Fix path to obtain RISC-V toolchain

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -5,7 +5,7 @@ sudo apt install -y sshpass
 # Get RISCV Toolchain
 mkdir riscv-tools
 export SSHPASS=$DEPLOY_PASS2
-sshpass -e scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -P $DEPLOY_PORT2 $DEPLOY_USER2@$DEPLOY_HOST:$DEPLOY_PATH2/../buntu-home/toolchain.tar.gz riscv-tools/
+sshpass -e scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -P $DEPLOY_PORT2 $DEPLOY_USER2@$DEPLOY_HOST:~/toolchain.tar.gz riscv-tools/
 
 cd riscv-tools/
 tar -xvzf toolchain.tar.gz
