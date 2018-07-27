02bd4d020eb3d036437b77ead2168b4f292e7f83
João Filipe <joao1996f@gmail.com>
2018-07-27 15:21:32 +0100

    Creation of chisel spec tests for ADD
    File reg_tests.scala will implement chisel spec tests of R-type instructions for the decoder. It implements already these tests for ADD instruction.

--- src/test/scala/decoder/decoderUnitTester.scala
+++ src/test/scala/decoder/decoderUnitTester.scala
@@ -6,15 +6,20 @@ import chisel3.iotesters.{ChiselFlatSpec, Driver, PeekPokeTester}
 import adept.config.AdeptConfig
 
 import adept.decoder.tests.imm._
+import adept.decoder.tests.reg._
 
 class DecoderTestBase(c: InstructionDecoder) extends PeekPokeTester(c) {
   val op_code = new OpCodes
   val slli = 1  //b001
+  val funct7alu = Integer.parseInt("0100000", 2);
 }
 
 class DecoderUnitTesterAll(e: InstructionDecoder) extends PeekPokeTester(e) {
     // Immediate Type Instructions
     new SLLI(e)
+
+    // Register Type Instructions
+    new ADD(e)    
 }
 
 class DecoderTester extends ChiselFlatSpec {
@@ -29,4 +34,13 @@ class DecoderTester extends ChiselFlatSpec {
       e => new SLLI(e)
     } should be (true)
   }
+
+  ////////////////////////////////////////////////////////////////////////////
+  // Register Type Instructions
+  ////////////////////////////////////////////////////////////////////////////
+  "Decoder" should s"test ADD instruction (with verilator)" in {
+    Driver(() => new InstructionDecoder(config), "verilator") {
+      e => new ADD(e)
+    } should be (true)
+  }  
 }
