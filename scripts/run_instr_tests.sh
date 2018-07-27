09a77b932afa76dd718daf151a249429cab18336
Luís Fiolhais <lfiolhais@iobundle.com>
2018-07-27 15:12:33 +0100

    Tests: Improve run instruction tests script

--- scripts/run_instr_tests.sh
+++ scripts/run_instr_tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ -z ${RISCV+x} ]; then
+if [ -z "${RISCV+x}" ]; then
     echo "The RISCV environment variable is not set. Please set it and rerun script."
     exit 1
 fi
@@ -14,15 +14,18 @@ RESULTS_FOLDER=$TEST_FILE_FOLDER/results/
 TOP=$(pwd)
 
 cd $TEST_FILE_FOLDER && make clean && make
-cd $TOP
+cd "$TOP"
 
-for test in $(ls $HEXS); do
-    LOG_FOLDER=logs/"$test"_log
-    mkdir -p $LOG_FOLDER
+for test in $HEXS/*; do
+    TEST_NAME=${test##*/}
+    LOG_FOLDER=logs/"$TEST_NAME"_log
+    mkdir -p "$LOG_FOLDER"
 
+    OUTPUT_TEST_FILE="$LOG_FOLDER/verilator_$test_$(date +%d-%m-%Y)"
+    OUTPUT_RESULT_FILE="$LOG_FOLDER/verilator_result_$TEST_NAME_$(date +%d-%m-%Y)"
     # Run test in verilator
-    make test-verilator PROG=$HEXS/$test > $LOG_FOLDER/verilator_$test_$(date +%d-%m-%Y)
+    make test-verilator PROG="$test" > "$OUTPUT_TEST_FILE"
     # Cut the log and take just the output that we want
-    cat $LOG_FOLDER/verilator_$test_$(date +%d-%m-%Y) | tac | grep "PC = " -m 1 -B32 | sed 's/\[.*\] \[.*\] //g' | tac > $LOG_FOLDER/verilator_result_$test_$(date +%d-%m-%Y)
-    diff -w $LOG_FOLDER/verilator_result_$test_$(date +%d-%m-%Y) $RESULTS_FOLDER/${test%.hex}/verilator
+    tac "$OUTPUT_TEST_FILE" | grep "PC = " -m 1 -B32 | sed 's/\[.*\] \[.*\] //g' | tac > "$OUTPUT_RESULT_FILE"
+    diff -w "$OUTPUT_RESULT_FILE" "$RESULTS_FOLDER/${TEST_NAME%.hex}/verilator"
 done
